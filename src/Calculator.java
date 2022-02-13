aa769e63880bde670032854e66e264c667976b2e
eebalboni <emilyebalboni@gmail.com>
2022-02-13 12:54:51 -0500

    prompting user to enter two ints and printing sum

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Calculator {
-    public static void main(String args){
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter an integer: ");
+        int numOne = sc.nextInt();
+        System.out.println("Please enter a second integer: ");
+        int numTwo = sc.nextInt();
+        System.out.println("The sum of the two numbers is: " + (numOne + numTwo));
         
     }
 }
